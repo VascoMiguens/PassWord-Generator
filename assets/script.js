cb971646b590931de4cea7a4cc82fd000f679980
VascoMiguens <v_miguens@hotmail.com>
2022-10-13 23:29:38 +0100

    - inserted user option arrays - map function to turn lowecase characters to uppercase

---
+++ assets/script.js
@@ -0,0 +1,85 @@
+// Assignment Code
+
+var generateBtn = document.querySelector("#generate");
+//Input Variables
+//Numbers Array
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//Special characters array
+var specialChars = [
+  "!",
+  "#",
+  "$",
+  "%",
+  "&",
+  "'",
+  "(",
+  ")",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "[",
+  "]",
+  "^",
+  "_",
+  "`",
+  "{",
+  "|",
+  "}",
+  "~",
+];
+//Lowercase Characters Array
+var lowerChars = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+//Uppercase Characters array
+//The map will pick every element of the array one
+//by one and will pass it as x to the function and
+//turn it into uppercase
+var upperChars = lowerChars.map(function (x) {
+  return x.toUpperCase;
+});
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
